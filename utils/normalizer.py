2448e9fefa5bebfc4bd2d267afb4355a78246af1
buildwithlogic <kumarvishal187@gmail.com>
2026-05-11 00:44:23 +0530

    Update normalizer.py

--- utils/normalizer.py
+++ utils/normalizer.py
@@ -1,1 +1,11 @@
-temp
+import re
+
+
+def normalize_to_tag(title: str) -> str:
+    cleaned = title.strip()
+    cleaned = re.sub(r"\s+", " ", cleaned)
+
+    words = cleaned.split(" ")
+    capitalized_words = [word.capitalize() for word in words if word]
+
+    return "#" + "".join(capitalized_words)
